3b9df299e14b7828316dac8196de48f52cb3cde8
drenloh135 <32531976+drenloh135@users.noreply.github.com>
2020-04-04 21:49:52 +0800

    Add files via upload
    Added a symmetric encryption function

--- passwordSaver.py
+++ passwordSaver.py
@@ -1,11 +1,35 @@
 import os 
-File = "PasswordSaver.txt" #Target file to be used
+import os.path
+from os import path
+import cryptography
+from cryptography.fernet import Fernet #Symmetric encrpytion is done here
+OutputFile = 'test.encrypted' #For encryption purposes
+TargetFile = "PasswordSaver.txt" #Target file to be used
 #File = "PasswordSaverReal.txt"
+
+init = 0 #flow control
+
+
+def GenerateKey(): #Ensures key is created/ready when app is launched
+    if path.exists('Filekey.key'): #Checks if key alr exists, so as not to replace existing key
+        print("Key is ready\n")
+    else:
+        CrpytKey = Fernet.generate_key()
+        file = open('FileKey.key', 'wb')
+        file.write(CrpytKey)
+        file.close()
+        print("Key has been generated\n")
+    file = open('FileKey.key', 'rb')
+    Key = file.read()
+    file.close()
+    return Key    #Key is returned in bytes
+
+        
 
 def ShowPassword(): #Shows a list of passwords stored
     print("Showing you a list of passwords saved ...\n")
     try:
-        with open(File, "r") as f:
+        with open(TargetFile, "r") as f:
             for x in f:
                 print(x)
     except FileNotFoundError:
@@ -19,11 +43,11 @@ def AddPassword(): #Allows user to add info about domain,user and PW. Creates fi
     print("What is the password?")
     pw = input()
 
-    with open(File, "a") as f:
+    with open(TargetFile, "a") as f:
         
         f.write("Domain: " + domain + '   ')
         f.write("User: " + user + '   ')
-        f.write("Password: " + pw + "\n\n")
+        f.write("Password: " + pw + "\n")
         
         print("Your infomation has been saved! \n")
 
@@ -34,7 +58,7 @@ def ClearFile(): #Completely deletes target file
     try:
         if int(ans) == 1:
             try: 
-                os.remove(File)
+                os.remove(TargetFile)
                 print("\nPasswords deleted \n")
             except FileNotFoundError:
                 print("File hasn't beeen created yet \n")
@@ -42,14 +66,44 @@ def ClearFile(): #Completely deletes target file
             print("Going back to main menu ... \n")
     except ValueError:
         print("Please type either 1 or 2. Returning you back to the main menu ...\n")
-    
 
+def EncryptFile(): #Encrypts file and saves it as test.encrypted
+    global TargetFile
+    global OutputFile
+    global Key
+
+    with open(TargetFile, 'rb') as f:
+        data = f.read()
+    fernet = Fernet(Key)
+    encrypted = fernet.encrypt(data)
+    open(OutputFile, 'x')
+    with open(OutputFile,'wb') as f:
+        f.write(encrypted)
+
+def DecryptFile(): #Decrypts test.encrypted and outputs in decrypted.txt
+    global TargetFile
+    global OutputFile
+    global Key
+    
+    with open(OutputFile, 'rb') as f:
+        data = f.read()
+    fernet = Fernet(Key)
+    decrypted = fernet.decrypt(data)
+    with open('decrypted.txt', 'wb') as f:
+        f.write(decrypted)
+    
 while (1):
+    if init == 0:
+        Key = GenerateKey()
+        init = 1
+    
     print("What would u like to do today?")
     print("1. See list of stored passwords")
     print("2. Add password")
     print("3. Clear passwords")
-    print("4. Exit application\n")
+    print("4. Encrpyt file")
+    print("5. Decrypt file")
+    print("6. Exit application\n")
     choice = int(input())
     try:
         if choice == 1:
@@ -62,6 +116,12 @@ while (1):
             ClearFile()
             
         elif choice == 4:
+            EncryptFile()
+        
+        elif choice == 5:
+            DecryptFile()
+
+        elif choice == 6:
             break
         else:
             print("There was a error. Pls try again")
